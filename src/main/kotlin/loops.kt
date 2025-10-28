a815d0a8814a296ca928e589bf4bad23d15666f0
sridhar-pc <sridharrajavel1506@gmail.com>
2025-10-29 00:12:21 +0530

    loops code updated

--- src/main/kotlin/loops.kt
+++ src/main/kotlin/loops.kt
@@ -1,6 +1,11 @@
 fun main(args: Array<String>) {
     val num = arrayOf(1, 5, 10, 15, 20)
     for (x in num) {
+        if (x % 2 == 0) {
+            println("pass")
+        } else {
+            println("fails")
+        }
         println(x)
     }
 }
